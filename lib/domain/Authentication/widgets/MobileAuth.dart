bccd65e5f581eb1027b1e225477644c043ce926d
SHAMIIT <shamiitltd@gmail.com>
2023-04-23 00:16:16 +0530

    authentication with firebase Mobile Authentication work.

--- lib/domain/Authentication/widgets/MobileAuth.dart
+++ lib/domain/Authentication/widgets/MobileAuth.dart
@@ -1,3 +1,4 @@
+import 'package:cng_navigator/config/Colors.dart';
 import 'package:flutter/material.dart';
 
 class MobileAuth extends StatefulWidget {
@@ -12,17 +13,56 @@ class _MobileAuthState extends State<MobileAuth> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        margin: const EdgeInsets.only(left: 25, right: 25),
         alignment: Alignment.center,
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children:  const [
-               Text('Phone Verification',
-                style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold),
-            ),
-              SizedBox(height: 8),
-              Text('Enter your 10 Digit Mobile Number',
-                style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),
+            children: [
+              // Display the logo image
+              Image.asset(
+                'assets/logo_login_page.png',
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(height: 8),
+
+              // Display the header text
+              const Text(
+                'Phone Verification',
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+
+              // Display the description text
+              const Text(
+                'We need to register your Mobile number before getting Started !',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 12),
+
+              // Display the phone number input field
+              Container(
+
+                child: Row(
+                  children: const [
+                    Expanded(
+                      child: TextField(
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 12),
+
+              // Display the "Get OTP" button
+              const SizedBox(
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: null, // TODO: Implement the "Get OTP" functionality
+                  child: Text("Get OTP"),
+                ),
               ),
             ],
           ),
--- lib/domain/Authentication/widgets/OtpVerification.dart
+++ lib/domain/Authentication/widgets/OtpVerification.dart
@@ -10,6 +10,33 @@ class OtpVerification extends StatefulWidget {
 class _OtpVerificationState extends State<OtpVerification> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(left: 25,right: 25),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:   const [
+              Text('OTP Verification',
+                style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text('We need to register your Mobile number before getting Started !',
+                style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500, ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 45,
+                child: ElevatedButton(onPressed: null,
+                  // style: ElevatedButton.icon(backgroundColor: Colors.blueGrey),
+                  child: Text("Get OTP"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
